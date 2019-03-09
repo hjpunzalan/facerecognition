5bc2c8d3ae5792844edcbc170bd9afec4df0ed02
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-03-09 16:58:27 +0800

    deploy

---
+++ build/precache-manifest.0947872ef959390a8ce12c85efd23451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c28bb55de6f42105bee",
+    "url": "/facerecognition/static/js/runtime~main.2c28bb55.js"
+  },
+  {
+    "revision": "2792706d216cb2b1d139",
+    "url": "/facerecognition/static/js/main.2792706d.chunk.js"
+  },
+  {
+    "revision": "127c33b8ff24922dda31",
+    "url": "/facerecognition/static/js/2.127c33b8.chunk.js"
+  },
+  {
+    "revision": "2792706d216cb2b1d139",
+    "url": "/facerecognition/static/css/main.093bc003.chunk.css"
+  },
+  {
+    "revision": "127c33b8ff24922dda31",
+    "url": "/facerecognition/static/css/2.e5c0d737.chunk.css"
+  },
+  {
+    "revision": "d594d0021453939dc6576e02527d0ea3",
+    "url": "/facerecognition/index.html"
+  }
+];
