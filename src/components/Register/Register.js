af650797e882319024d85446331a17583f4a4668
hjpunzalan <47600145+hjpunzalan@users.noreply.github.com>
2019-04-24 15:17:19 +0800

    went back to https://quiet-atoll-12771.herokuapp

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
   }
 
   onSubmitRegister = () => {
-    fetch('https://facerecognitionhjpunzalan.herokuapp.com/register', {
+    fetch('https://quiet-atoll-12771.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type':'application/json'}, // has the dash so it has to be in quotes
       body:JSON.stringify ({ //sending to server has to be in JSON and not javascript object
